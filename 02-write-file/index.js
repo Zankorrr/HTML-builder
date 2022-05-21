c98246b1071bdc11b1e92d459c86d5792c21e882
Zankorrr <zankor92@gmail.com>
2022-05-21 17:44:14 +0300

    feat: second task is done

---
+++ 02-write-file/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+const { stdin, stdout, exit } = require("process");
+
+stdout.write("Hi, type something and press Enter\n");
+stdin.on("data", (data) => {
+  let str = data.toString().trim();
+  if (str == "exit") {
+    stdout.write("Well, goodbye\n");
+    exit();
+  }
+  fs.appendFile(path.join(__dirname, "text.txt"), data, () => {
+    stdout.write("Anything else?\n");
+  });
+});
+process.on("SIGINT", () => {
+  stdout.write("\nWell, goodbye\n");
+  exit();
+});
