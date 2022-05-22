eed4c9a4a516fa639405932f8f9d823543d3b1a5
Zankorrr <zankor92@gmail.com>
2022-05-22 16:06:59 +0300

    feat: fourth task is done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const path = require("path");
+const fs = require("fs");
+const { stat } = require("fs");
 
+fs.rm(path.join(__dirname, "files-copy"), { recursive: true }, () => {
+  fs.promises
+    .mkdir(path.join(__dirname, "files-copy"), { recursive: true })
+    .then();
+  fs.promises
+    .readdir(path.join(__dirname, "files"), { withFileTypes: true })
+    .then((files) => {
+      for (let file of files) {
+        stat(path.join(__dirname, "files", file.name), () => {
+          fs.promises.copyFile(
+            path.join(__dirname, "files", file.name),
+            path.join(__dirname, "files-copy", file.name)
+          );
+        });
+      }
+    });
+});
